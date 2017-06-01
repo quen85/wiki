fc103fe096000284e60153f39826a6dd84cad41b
Unknown <nicolaslebret50@gmail.com>
2017-06-01 10:16:04 +0200

    Views_V2

--- web/js/app.js
+++ web/js/app.js
@@ -1,39 +1,5 @@
-var app = angular.module("WikiApp", ["ngRoute"]);
+var wikiApp = angular.module('wikiApp', []);
 
-app.config(['$routeProvider', function($routeProvider){
-    $routeProvider.
-    when('/', {
-        templateUrl: '/templates/index.html',
-        controller: "FrontController",
-        controllerAs: 'ctrl',
-        title: 'homepage'
-    })
-}]);
+wikiApp.controller('wikiCtrl', ['$scope', function ($scope) {
 
-app.run(['$rootScope', '$route', function($rootScope, $route) {
-    $rootScope.$route = $route;
-    $rootScope.$on('$routeChangeSuccess', function() {
-        // Si on veux faire des trucs au changement de page
-    });
-}]);
-
-app.directive('header', ['$http', function($http){
-    return {
-        restrict: 'EA',
-        templateUrl: '/templates/bases/header.html'
-    }
-}]);
-
-app.directive('footer', ['$http', function($http){
-    return {
-        restrict: 'EA',
-        templateUrl: '/templates/bases/footer.html'
-    }
-}]);
-
-app.directive('titre', ['$http', function($http){
-    return {
-        restrict: 'EA',
-        templateUrl: '/templates/bases/title.html'
-    }
 }]);
