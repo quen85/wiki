e12253c8524f1f184147dfe8d62da6d276748b4d
Unknown <nicolaslebret50@gmail.com>
2017-06-01 17:18:28 +0200

    login

--- web/js/controller/frontcontroller.js
+++ web/js/controller/frontcontroller.js
@@ -1,22 +1,45 @@
 app.controller('FrontController',
-    ['$scope', '$http',
-        function($scope, $http){
-        	var ctrl=this;
-        	ctrl.signup=function({
-        		console.log($scope.email)
+    ['$scope', '$http', '$location',
+        function($scope, $http, $location){
+        	// var ctrl=this
+        	$scope.signup=function(){
+        		// console.log($scope.email)
         		var req = {
-				 method: 'POST',
-				 url: 'http://127.0.0.1:8000/#!/register.html',
-				 headers: {
-				   'Content-Type': undefined
-				 },
-				 data: { pseudo: $scope.pseudo,email: $scope.email, password: $scope.password
-					}
-				}
-
-				$http(req).then(function(){...}, function(){...});
-        	})
+    				 method: 'POST',
+    				 url: '/signup',
+    				 headers: {
+    				   'Content-Type': 'application/json'
+    				 },
+    				 data: {
+               username: $scope.pseudo,
+               email: $scope.email,
+               password: $scope.password
+    					}
+    				}
+    				$http(req).then(function(){
+              $location.url('/')
+            }, function(){});
+          }
+          $scope.login=function(){
+        		var req = {
+    				 method: 'POST',
+    				 url: '/login_check',
+    				 headers: {
+    				   'Content-Type': 'application/json'
+    				 },
+    				 data: {
+               _username: $scope.pseudo,
+               _password: $scope.password,
+               _target_path: '/',
+               _failure_path: '/login'
+    					}
+    				}
+    				$http(req).then(function(response){
+              console.log(response)
+            }, function(response){
+              console.log(response)
+            });
+          }
         }
     ]
 );
-
