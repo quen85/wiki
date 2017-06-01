696f7d66b7796c1593eb71afd8a2a8887e4af462
jordan77 <jordan.desjardin@gmail.com>
2017-06-01 15:51:01 +0200

    form

--- web/js/controller/frontcontroller.js
+++ web/js/controller/frontcontroller.js
@@ -1,6 +1,22 @@
 app.controller('FrontController',
     ['$scope', '$http',
         function($scope, $http){
+        	var ctrl=this;
+        	ctrl.signup=function({
+        		console.log($scope.email)
+        		var req = {
+				 method: 'POST',
+				 url: 'http://127.0.0.1:8000/#!/register.html',
+				 headers: {
+				   'Content-Type': undefined
+				 },
+				 data: { pseudo: $scope.pseudo,email: $scope.email, password: $scope.password
+					}
+				}
+
+				$http(req).then(function(){...}, function(){...});
+        	})
         }
     ]
 );
+
